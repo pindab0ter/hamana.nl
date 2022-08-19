6c1c641bca8813168eeac747ce0a9e57401b0133
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-08-19 19:09:37 +0200

    Set default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
@@ -7,6 +9,9 @@ module.exports = {
         brown: '#716056',
         orange: '#D49635',
         beige: '#F4F2F2',
+      },
+      fontFamily: {
+        'sans': ['Open Sans', ...defaultTheme.fontFamily.sans],
       }
     },
   },
