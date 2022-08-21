fcd1a138dfd48b475d84ff8946593411e6b9b277
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-08-21 12:06:19 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+USER=hamana.nl
+HOST=hamana.nl
+PORT=22141
+DIR=/httpdocs/
+
+hugo
+rsync \
+	--verbose \
+	--archive \
+	--compress \
+	--delete \
+	--rsh "ssh -p $PORT" \
+	public/ \
+	"${USER}@${HOST}:${DIR}"
+
+exit 0
